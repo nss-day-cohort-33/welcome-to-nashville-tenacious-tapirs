a495a3b6f97c993d7782cb3820d17d43413ee3f2
Curt Cato <ccato829@gmail.com>
2019-06-11 15:59:12 -0500

    fixed keys template

--- src/scripts/keys.template.js
+++ src/scripts/keys.template.js
@@ -1,4 +1,16 @@
-const app_keys = {
-    app_id: <your ID goes here>,
-    app_key:<your key goes here>
+const parksAppKeys = {
+    appId: "your ID goes here",
+    appKey:"your Key goes here"
+}
+const restaurantsAppKeys = {
+    appId:"your ID goes here",
+    appKey:"your key goes here"
+}
+const meetupsAppKeys = {
+    appId: "your ID goes here",
+    appKey:"your key goes here"
+}
+const concertsAppKeys = {
+    appId: "your ID goes here",
+    appKey:"your key goes here"
 }
