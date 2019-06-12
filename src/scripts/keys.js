11e607f9fb4dac3ef7e887040e16adb26b2420ee
Curt Cato <ccato829@gmail.com>
2019-06-12 16:02:18 -0500

    restaurant search results added to DOM

---
+++ src/scripts/keys.js
@@ -0,0 +1,1 @@
+const restaurantsAppKeys = "b705006b1839e722cff5e22a5fc4241f"
---
+++ src/scripts/restaurants/components.js
@@ -0,0 +1,22 @@
+// event listener for restaurant form
+
+document.querySelector("#searchRestaurantsBtn").addEventListener("click", event => {
+    let searchInput = document.querySelector("#searchRestaurants").value
+    document.querySelector("#restaurantsContainer").innerHTML = ""
+    addAPIData(searchInput)
+    })
+
+
+
+function addRestaurantsToDom(restaurantComponent) {
+    document.querySelector("#restaurantsContainer").innerHTML += restaurantComponent
+}
+
+function createRestaurantsSearchComponent(restaurantsObj) {
+    return `
+    <div>
+    <p>${restaurantsObj.name}: ${restaurantsObj.location.address} <button id = "restaurantsSaveBtn">Save</button></p>
+    </div>    
+    `
+}
+
