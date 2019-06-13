a29d0824f42f7eb9979d938f053d5541433c0b1d
berkeley platte <berkeleyplatte989@gmail.com>
2019-06-13 09:38:25 -0500

    made a minor change I commentted out, shouldn't affect functionability

--- src/scripts/concerts/api.js
+++ src/scripts/concerts/api.js
@@ -10,28 +10,29 @@ function addApiData(concertItem) {
   getApiConcert(concertItem).then(concertArr => {
     console.log(concertArr);
 
-    // for (i = 0; i < 4; i++) {
-    //   let result = `<p>${i + 1}. ${concertArr._embedded.events[i].name} at ${
-    //     concertArr._embedded.events[i]._embedded.venues[0].name
-    //   } <button id="addConcertsBtn">Add</button></p>`;
-    //   document.querySelector("#concertsContainer").innerHTML += result;
-    // }
-        let searchResults = `
-    <ol>
-    <li>${concertArr._embedded.events[0].name} at ${
-          concertArr._embedded.events[0]._embedded.venues[0].name
-        } <button id="addConcertsBtn">Add</button></li>
-    <li>${concertArr._embedded.events[1].name} at ${
-          concertArr._embedded.events[1]._embedded.venues[0].name
-        } <button id="addConcertsBtn">Add</button></li>
-    <li>${concertArr._embedded.events[2].name} at ${
-          concertArr._embedded.events[2]._embedded.venues[0].name
-        } <button id="addConcertsBtn">Add</button></li>
-    <li>${concertArr._embedded.events[3].name} at ${
-          concertArr._embedded.events[3]._embedded.venues[0].name
-        } <button id="addConcertsBtn">Add</button></li>
-    </ol>
-    `;
-    document.querySelector("#concertsContainer").innerHTML = searchResults;
+    for (i = 0; i < 4; i++) {
+      let result = `<p>${i + 1}. ${concertArr._embedded.events[i].name} at ${
+        concertArr._embedded.events[i]._embedded.venues[0].name
+      } <button id="addConcertsBtn">Add</button></p>`;
+      // if {concertArr._embedded}
+      document.querySelector("#concertsContainer").innerHTML += result;
+    }
+    //     let searchResults = `
+    // <ol>
+    // <li>${concertArr._embedded.events[0].name} at ${
+    //       concertArr._embedded.events[0]._embedded.venues[0].name
+    //     } <button id="addConcertsBtn">Add</button></li>
+    // <li>${concertArr._embedded.events[1].name} at ${
+    //       concertArr._embedded.events[1]._embedded.venues[0].name
+    //     } <button id="addConcertsBtn">Add</button></li>
+    // <li>${concertArr._embedded.events[2].name} at ${
+    //       concertArr._embedded.events[2]._embedded.venues[0].name
+    //     } <button id="addConcertsBtn">Add</button></li>
+    // <li>${concertArr._embedded.events[3].name} at ${
+    //       concertArr._embedded.events[3]._embedded.venues[0].name
+    //     } <button id="addConcertsBtn">Add</button></li>
+    // </ol>
+    // `;
+    // document.querySelector("#concertsContainer").innerHTML = searchResults;
   });
 }
