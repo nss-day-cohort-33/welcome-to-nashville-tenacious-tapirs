f8122eb2c867a0e295a4ff0044cd1607974da789
berkeley platte <berkeleyplatte989@gmail.com>
2019-06-13 15:03:11 -0500

    got everything working

--- src/scripts/concerts/api.js
+++ src/scripts/concerts/api.js
@@ -1,6 +1,6 @@
 function getApiConcert(genre) {
   return fetch(
-    `https://app.ticketmaster.com/discovery/v2/events.json?apikey=${
+    `https://app.ticketmaster.com/discovery/v2/events.json?city=Nashville&countryCode=US&apikey=${
       concertsAppKeys.appKey
     }&classificationName=${genre}`
   ).then(concertFromApi => concertFromApi.json());
@@ -9,36 +9,24 @@ function getApiConcert(genre) {
 function addApiData(concertItem) {
   getApiConcert(concertItem).then(concertArr => {
     console.log(concertArr);
-
-    for (i = 0; i < 4; i++) {
-      let result = `<p>${i + 1}. ${concertArr._embedded.events[i].name} at ${
-        concertArr._embedded.events[i]._embedded.venues[0].name
-      } <button id="addConcertsBtn">Save</button></p>`;
-      if (
-        concertArr._embedded.events[i]._embedded.venues[0].city.name.value ===
-        "Nashville"
-      )
-        console.log(
-          concertArr._embedded.events[i]._embedded.venues[0].city.name
-        );
-      document.querySelector("#concertsContainer").innerHTML += result;
-    }
-    //     let searchResults = `
-    // <ol>
-    // <li>${concertArr._embedded.events[0].name} at ${
-    //       concertArr._embedded.events[0]._embedded.venues[0].name
-    //     } <button id="addConcertsBtn">Save</button></li>
-    // <li>${concertArr._embedded.events[1].name} at ${
-    //       concertArr._embedded.events[1]._embedded.venues[0].name
-    //     } <button id="addConcertsBtn">Save</button></li>
-    // <li>${concertArr._embedded.events[2].name} at ${
-    //       concertArr._embedded.events[2]._embedded.venues[0].name
-    //     } <button id="addConcertsBtn">Save</button></li>
-    // <li>${concertArr._embedded.events[3].name} at ${
-    //       concertArr._embedded.events[3]._embedded.venues[0].name
-    //     } <button id="addConcertsBtn">Save</button></li>
-    // </ol>
-    // `;
-    // document.querySelector("#concertsContainer").innerHTML = searchResults;
+    let searchResults = `
+    <ol>
+    <li>${concertArr._embedded.events[0].name} at ${
+      concertArr._embedded.events[0]._embedded.venues[0].name
+    } <button id="addConcertsBtn" class="saveConcertBtn">Save</button></li>
+    <li>${concertArr._embedded.events[1].name} at ${
+      concertArr._embedded.events[1]._embedded.venues[0].name
+    } <button id="addConcertsBtn" class="saveConcertBtn">Save</button></li>
+    <li>${concertArr._embedded.events[2].name} at ${
+      concertArr._embedded.events[2]._embedded.venues[0].name
+    } <button id="addConcertsBtn" class="saveConcertBtn">Save</button></li>
+    <li>${concertArr._embedded.events[3].name} at ${
+      concertArr._embedded.events[3]._embedded.venues[0].name
+    } <button id="addConcertsBtn" class="saveConcertBtn">Save</button></li>
+    </ol>
+    `;
+    document.querySelector("#concertsContainer").innerHTML = searchResults;
+    makeBtnWrk();
+    console.log(document.querySelectorAll(".saveConcertBtn"));
   });
 }
--- src/scripts/concerts/main.js
+++ src/scripts/concerts/main.js
@@ -1,6 +1,9 @@
-
-let addBtn = document.querySelector("addConcertsBtn");
-addBtn.addEventListener("click", () => {
-  document.querySelector("#concertsItenerary").innerHTML =
-    addBtn.parentElement.innerHTML;
-});
+function makeBtnWrk() {
+  let saveBtns = document.querySelectorAll(".saveConcertBtn");
+  for (let i = 0; i < saveBtns.length; i++) {
+    saveBtns[i].addEventListener("click", () => {
+      document.querySelector("#concertsItenerary").innerHTML =
+        saveBtns[i].parentElement.innerHTML;
+    });
+  }
+}
