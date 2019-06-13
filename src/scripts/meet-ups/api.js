17ec8b5bb562d77f86de943fd3845b83879d4c10
Jeff Hill <jeffhill1978@gmail.com>
2019-06-13 15:04:52 -0500

    final meetups code

--- src/scripts/meet-ups/api.js
+++ src/scripts/meet-ups/api.js
@@ -10,6 +10,7 @@ document.querySelector("#searchMeetupsBtn").addEventListener("click", event => {
   console.log(searchInput.value);
 
   // Call my API and insert the category id variable to return results only for that category
+//  function callMyAPI() {
   fetch(
     `https://www.eventbriteapi.com/v3/events/search/?categories=${
       searchInput.value
@@ -21,17 +22,35 @@ document.querySelector("#searchMeetupsBtn").addEventListener("click", event => {
       console.log(eventsFromAPI);
 
       // Iterate the API json events results and use as the parameter for the function argument that displays the results as HTML
-      for (let i = 0; i <= eventsFromAPI.events.length; i++) {
+      for (let i = 0; i < eventsFromAPI.events.length; i++) {
         console.log(eventsFromAPI.events[i]);
 
         // if else statement to look for "empty" events so they are not populated in the list
         if (eventsFromAPI.events[i].name.text === "test") {
-          eventsFromAPI.events[i] = "Event Unlisted";
-        } else
-          meetupsResultsContainer.innerHTML += meetupsResultsList(
-            eventsFromAPI.events[i]
-          );
+          eventsFromAPI.events[i].name.text = "Event Unlisted";
+        }
+        // This connects where I want my results to be displayed with the function that generates the string of HTML
+        // the paramter being passed into the "Results" function is the results of the API fetch call loop
+        meetupsResultsContainer.innerHTML += meetupsResultsList(eventsFromAPI.events[i], i);
       }
+
+    // Declare a variable to select the class .saveBtn that is attached to the button element in the Results div.
+    //  Loop through the class .saveBtn and add it to each of the results populated in the results container.
+     const saveButtons = document.querySelectorAll(".saveBtn")
+     for (let i = 0; i < saveButtons.length; i++) {
+         console.log(saveButtons[i])
+
+        //  Add a click event listener to each newly generated save button
+         saveButtons[i].addEventListener("click", (event) => {
+             console.log("button got clicked", event.target.id)
+            //  call the function defined on components.js and pass the argument that will target the
+            // id #results that was added to the H2 tag of the html string.
+            // The event.target.id.split means that we want to split off the ${} and add it to the #results of the h2 tag
+            AddMeetupToItinerary(document.querySelector(`#results-${event.target.id.split("-")[1]}`))
+         })
+     }
     });
 });
+
+
 
--- src/scripts/meet-ups/components.js
+++ src/scripts/meet-ups/components.js
@@ -1,24 +1,28 @@
-const meetupsResultsList = function(eventsList) {
+const meetupsResultsList = function(eventsList, index) {
+    // In order to be able to target the button and add the click event listener and target the h2 to be injected
+    // as HTML into the Itinerary container we need to add a new parameter ${} which will add a number to the
+    // element IDs.
   return `
-    <div>
-<h2>Event Name: ${eventsList.name.text}</h2>
-<h3>Event Description: ${eventsList.summary}</h3>
-<a href="${eventsList.url}" target="_blank">Event URL</a>
-<button class="meetupsResultsBtn"> Save </button>
-</div>
-    `;
+    <div >
+
+        <h2 id="results-${index}">Event Name: ${eventsList.name.text}</h2>
+        <h3>Event Description: ${eventsList.summary}</h3>
+        <a href="${eventsList.url}" target="_blank">Event URL</a>
+        <button class="saveBtn" id="meetupsResultsBtn-${index}"> Save </button>
+    </div>
+    `
 };
 
-// target Save button in MeetUp Results events list Event Handler or Event Bubbling
-const meetupSaveToItinerary = document.querySelector("#meetupsItenerary")
-meetupSaveToItinerary.addEventListener("click", event => {
-meetupSaveToItinerary.innerHTML = meetupsResultsList
-});
-// .addEventListener("click", event =>{}
-//     // meetupsResultsContainer.innerHTML = "";
 
-//     // Display Saved event in My Itiniary
-//     meetupsResultsContainer.innerHTML += meetupsResultsList(
-//       eventsFromAPI.events[i]
-//     );
+// Define a function that will hold one argument. The function will select the Itinerary DOM Container
+// to have html inserted into it when the Save button is clicked.
+// The Itinerary DOM element html will = the html of the selected DOM element from the results HTML string
+const AddMeetupToItinerary = function(element) {
+    console.log("function being called", element)
+    document.querySelector("#meetupsItenerary").innerHTML = element.innerHTML
+}
+
+
+
+
 
