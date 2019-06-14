af3fdee5c08a5a6f68257dd9ac46dead32d28f1a
berkeley platte <berkeleyplatte989@gmail.com>
2019-06-14 08:55:48 -0500

    save button gone from itinerary

--- src/scripts/concerts/main.js
+++ src/scripts/concerts/main.js
@@ -3,7 +3,13 @@ function makeBtnWrk() {
   for (let i = 0; i < saveBtns.length; i++) {
     saveBtns[i].addEventListener("click", () => {
       document.querySelector("#concertsItenerary").innerHTML =
-        saveBtns[i].parentElement.innerHTML;
+        saveBtns[i].parentElement.firstChild.innerHTML;
+
+      rmvBtn();
     });
   }
+}
+function rmvBtn() {
+  var btn = document.querySelector("addConcertsBtn");
+  btn.parentNode.removeChild(btn);
 }
